2c8b395a11bb32c99f4cb704b5fb383e21ff2076
spnikit <spnikit@gmail.com>
2022-09-06 12:48:16 +0300

    homework2 initial

--- src/main/java/com/spnikit/homework1/taks2/Task2.java
+++ src/main/java/com/spnikit/homework1/taks2/Task2.java
@@ -1,4 +1,4 @@
-package com.spnikit.taks2;
+package com.spnikit.homework1.taks2;
 
 import java.util.Arrays;
 
@@ -10,7 +10,7 @@ public class Task2 {
         System.out.println("Начальный массив: " + Arrays.toString(arr));
         insertionSort(arr);
 
-        assert Arrays.equals(arr, new int[]{1, 2, 3, 4, 5, 5, 6, 9}) : "Not Equal";
+//        assert Arrays.equals(arr, new int[]{1, 2, 3, 4, 5, 5, 6, 9}) : "Not Equal";
 
         System.out.println("Отсортированный массив: " + Arrays.toString(arr));
     }
@@ -20,13 +20,13 @@ public class Task2 {
 
 
         for (int i = 1; i < array.length; i++) {
+
             int tmp = array[i]; // положили во временную переменную
-            int j = i - 1;
-            while (j >= 0) {
+            int j;
+            for (j = i - 1; j >= 0; j--) {
 
                 if (array[j] > tmp) { // если число слева от сохраненной переменной больше ее, передвигаем
                     array[j + 1] = array[j];
-                    j--;
                 } else {
                     break; // если меньше или равно прерываем цикл
                 }
